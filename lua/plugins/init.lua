00381b1a783687db5ce0214abb0efc1f9d4093bb
asep komarudin <pojokcodeid@gmail.com>
2023-01-15 07:34:41 +0700

    add new plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -283,4 +283,38 @@ return {
 			require("user.gitsigns")
 		end,
 	},
+
+	-- additional new plugins
+	-- active indent guide and indent text objects
+	{
+		"echasnovski/mini.indentscope",
+		version = false, -- wait till new 0.7.0 release to put it back on semver
+		event = "BufReadPre",
+		opts = {
+			-- symbol = "▏",
+			symbol = "│",
+			options = { try_as_border = true },
+		},
+		config = function(_, opts)
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+				callback = function()
+					vim.b.miniindentscope_disable = true
+				end,
+			})
+			require("mini.indentscope").setup(opts)
+		end,
+	},
+	-- makes some plugins dot-repeatable like leap
+	{ "tpope/vim-repeat", event = "VeryLazy" },
+	-- better diagnostics list and others
+	{
+		"folke/trouble.nvim",
+		cmd = { "TroubleToggle", "Trouble" },
+		opts = { use_diagnostic_signs = true },
+		keys = {
+			{ "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+			{ "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+		},
+	},
 }
