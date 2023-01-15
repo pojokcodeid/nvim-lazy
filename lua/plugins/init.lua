34f207c5db5a8795bfa0c3e1645016f7ab0d6972
asep komarudin <pojokcodeid@gmail.com>
2023-01-15 11:30:38 +0700

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -255,14 +255,45 @@ return {
 		config = function()
 			local wilder = require("wilder")
 			wilder.setup({ modes = { ":", "/", "?" } })
-			wilder.set_option(
-				"renderer",
-				wilder.popupmenu_renderer({
-					highlighter = wilder.basic_highlighter(),
+			-- *ini popup biasa
+			-- wilder.set_option(
+			-- 	"renderer",
+			-- 	wilder.popupmenu_renderer({
+			-- 		highlighter = wilder.basic_highlighter(),
+			-- 		left = { " ", wilder.popupmenu_devicons() },
+			-- 		right = { " ", wilder.popupmenu_scrollbar() },
+			-- 	})
+			-- )
+
+			-- *ini untuk border rounded
+			-- wilder.set_option('renderer', wilder.popupmenu_renderer(
+			-- 	wilder.popupmenu_border_theme({
+			-- 		highlights = {
+			-- 			border = 'Normal', -- highlight to use for the border
+			-- 		},
+			-- 		-- 'single', 'double', 'rounded' or 'solid'
+			-- 		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+			-- 		border = 'rounded',
+			-- 		left = { " ", wilder.popupmenu_devicons() },
+			--     right = { " ", wilder.popupmenu_scrollbar() },
+			-- 	})
+			-- ))
+
+			-- *ini untuk popup dialog
+			wilder.set_option('renderer', wilder.popupmenu_renderer(
+				wilder.popupmenu_palette_theme({
+					-- 'single', 'double', 'rounded' or 'solid'
+					-- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+					border = 'rounded',
+					max_height = '40%',      -- max height of the palette
+					min_height = 0,          -- set to the same as 'max_height' for a fixed height window
+					prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+					reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
 					left = { " ", wilder.popupmenu_devicons() },
-					right = { " ", wilder.popupmenu_scrollbar() },
+			    right = { " ", wilder.popupmenu_scrollbar() },
+					pumblend = 20,
 				})
-			)
+			))
 		end,
 	},
 	{
