d0298906e615847f3285ab48e6a86bfdc38b852d
asep komarudin <pojokcodeid@gmail.com>
2023-01-17 13:48:54 +0700

    update width

--- lua/user/dressing.lua
+++ lua/user/dressing.lua
@@ -7,9 +7,9 @@ dressing.setup({
 	input = {
 		default_prompt = "➤ ",
 		win_options = { winhighlight = "Normal:Normal,NormalNC:Normal" },
-		prefer_width = 50,
+		prefer_width = 30,
 		max_width = { 140, 0.9 },
-		min_width = { 50, 0.2 },
+		min_width = { 30, 0.2 },
 	},
 	select = {
 		backend = { "telescope", "builtin" },
