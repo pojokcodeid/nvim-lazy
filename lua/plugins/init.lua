fe32afb482d6bd8fd57f1c8095fac2b6a54791bc
asep komarudin <pojokcodeid@gmail.com>
2023-01-21 10:31:05 +0700

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
-	-- the colorscheme should be available when starting Neovim
 	{
 		"nvim-lua/plenary.nvim",
 		commit = "4b7e52044bbb84242158d977a50c4cbcd85070c7",
@@ -243,7 +242,7 @@ return {
 	},
 	{
 		"karb94/neoscroll.nvim",
-		event = "BufRead",
+		event = "WinScrolled",
 		config = function()
 			require("user.neoscroll")
 		end,
@@ -272,36 +271,37 @@ return {
 			-- )
 
 			-- *ini untuk border rounded
-			-- wilder.set_option('renderer', wilder.popupmenu_renderer(
-			-- 	wilder.popupmenu_border_theme({
-			-- 		highlights = {
-			-- 			border = 'Normal', -- highlight to use for the border
-			-- 		},
-			-- 		-- 'single', 'double', 'rounded' or 'solid'
-			-- 		-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-			-- 		border = 'rounded',
-			-- 		left = { " ", wilder.popupmenu_devicons() },
-			--     right = { " ", wilder.popupmenu_scrollbar() },
-			-- 	})
-			-- ))
-
-			-- *ini untuk popup dialog
 			wilder.set_option(
 				"renderer",
-				wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
+				wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+					highlights = {
+						border = "Normal", -- highlight to use for the border
+					},
 					-- 'single', 'double', 'rounded' or 'solid'
-					-- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+					-- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
 					border = "rounded",
-					max_height = "40%", -- max height of the palette
-					max_width = "40%",
-					min_height = 0, -- set to the same as 'max_height' for a fixed height window
-					prompt_position = "top", -- 'top' or 'bottom' to set the location of the prompt
-					reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
 					left = { " ", wilder.popupmenu_devicons() },
 					right = { " ", wilder.popupmenu_scrollbar() },
-					pumblend = 20,
 				}))
 			)
+
+			-- *ini untuk popup dialog
+			-- wilder.set_option(
+			-- 	"renderer",
+			-- 	wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
+			-- 		-- 'single', 'double', 'rounded' or 'solid'
+			-- 		-- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+			-- 		border = "rounded",
+			-- 		max_height = "40%", -- max height of the palette
+			-- 		max_width = "40%",
+			-- 		min_height = 0, -- set to the same as 'max_height' for a fixed height window
+			-- 		prompt_position = "top", -- 'top' or 'bottom' to set the location of the prompt
+			-- 		reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+			-- 		left = { " ", wilder.popupmenu_devicons() },
+			-- 		right = { " ", wilder.popupmenu_scrollbar() },
+			-- 		pumblend = 20,
+			-- 	}))
+			-- )
 		end,
 	},
 	{
