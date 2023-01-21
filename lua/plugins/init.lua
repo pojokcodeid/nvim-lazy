000afca700b7aab564fab09da6b38cd63c82c03f
asep komarudin <pojokcodeid@gmail.com>
2023-01-21 18:49:12 +0700

    update color scheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,7 +16,7 @@ return {
 		"navarasu/onedark.nvim",
 		init = function()
 			require("onedark").setup({
-				style = "darker",
+				style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
 			})
 			require("onedark").load()
 		end,
