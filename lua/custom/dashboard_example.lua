464560589225cba72974e28cb915213b3b093db0
asep komarudin <pojokcodeid@gmail.com>
2023-01-25 17:05:35 +0700

    update

---
+++ lua/custom/dashboard_example.lua
@@ -0,0 +1,16 @@
+local M = {}
+
+-- Untuk Ubah Dashboard
+-- 1. Rename File ini dari dashboard_exsample.lua menjadi dashboard.lua
+-- 2. Ubah dashboard sesuai keinginan dan kebutuhan
+
+M.dashboard = {
+	[[                               __                ]],
+	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+}
+
+return M
