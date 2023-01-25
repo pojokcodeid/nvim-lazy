eb90dc89654d1c244aac9385d032d2825fa9d56d
asep komarudin <pojokcodeid@gmail.com>
2023-01-26 06:40:43 +0700

    update

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,7 +14,7 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		--formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.prettier,
+		--formatting.prettier,
 		-- formatting.prettierd,
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua,
