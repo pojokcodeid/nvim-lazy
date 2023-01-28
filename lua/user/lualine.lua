84055f2ebe2db861a092b667fa0f06433291ea51
asep komarudin <pojokcodeid@gmail.com>
2023-01-28 16:06:42 +0700

    update style lualine

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -173,8 +173,13 @@ lualine.setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
+		--component_separators = { left = "", right = "" },
+		--section_separators = { left = "", right = "" },
+		--component_separators = { left = "", right = "" },
+		--section_separators = { left = "", right = "" },
 		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		section_separators = { left = " ", right = " " },
+
 		disabled_filetypes = {
 			"TelescopePrompt",
 			"packer",
