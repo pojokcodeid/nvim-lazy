539b72482a462dda6db1decb76a42367c0f80437
asep komarudin <pojokcodeid@gmail.com>
2023-01-31 00:08:00 +0700

    update telecope config

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -59,6 +59,41 @@ local options = {
 
 	extensions_list = { "themes", "terms" },
 
+	pickers = {
+		find_files = {
+			hidden = true,
+		},
+		live_grep = {
+			--@usage don't include the filename in the search results
+			only_sort_text = true,
+		},
+		grep_string = {
+			only_sort_text = true,
+		},
+		buffers = {
+			initial_mode = "normal",
+			mappings = {
+				i = {
+					["<C-d>"] = actions.delete_buffer,
+				},
+				n = {
+					["dd"] = actions.delete_buffer,
+				},
+			},
+		},
+		planets = {
+			show_pluto = true,
+			show_moon = true,
+		},
+		git_files = {
+			hidden = true,
+			show_untracked = true,
+		},
+		colorscheme = {
+			enable_preview = true,
+		},
+	},
+
 	mappings = {
 		i = {
 			["<C-n>"] = actions.cycle_history_next,
