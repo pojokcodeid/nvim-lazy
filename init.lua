6fb52c0e1c6815309d5c2f5ee688e22f0005f884
asep komarudin <pojokcodeid@gmail.com>
2023-02-12 17:00:07 +0700

    update config

--- init.lua
+++ init.lua
@@ -1,12 +1,1 @@
-local onsave = true
-require("user.options")
-require("config.lazy")
-require("user.keymaps")
-require("user.autocommands")
-require("user.colorscheme")
-require("user.snip")
-require("user.bufferline")
-if onsave then
-	require("user.format_onsave")
-end
--- require("user.chat_gpt")
+require("core")
