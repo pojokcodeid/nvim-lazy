e9febd9f568c12b9872151ec165a89e3627a0498
asep komarudin <pojokcodeid@gmail.com>
2023-02-12 17:08:02 +0700

    update lsp register

--- lua/custom/register_lsp.lua
+++ lua/custom/register_lsp.lua
@@ -1,3 +1,5 @@
+-- untuk referesi support language kunjungi link dibawah
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local M = {
 	"bashls",
 	"yamlls",
@@ -8,5 +10,6 @@ local M = {
 	"clangd",
 	"dartls",
 	"kotlin_language_server",
+	-- tambahkan di bawah sini setelah melakukan :MasonInstall
 }
 return M
