256d7305e859beb8b8c04858918ea9431447989f
asep komarudin <pojokcodeid@gmail.com>
2023-02-15 09:07:41 +0700

    update config

--- lua/user/dapui.lua
+++ lua/user/dapui.lua
@@ -1,6 +1,7 @@
 local dap, dapui = require("dap"), require("dapui")
 
 dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.setup()
 	dapui.open()
 end
 dap.listeners.before.event_terminated["dapui_config"] = function()
@@ -9,6 +10,5 @@ end
 dap.listeners.before.event_exited["dapui_config"] = function()
 	dapui.close()
 end
-dapui.setup()
 vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
 vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
