d66b72ede924136728d5320363af626adcf85bee
asep komarudin <pojokcodeid@gmail.com>
2023-02-19 16:41:27 +0700

    update

--- lua/user/format.lua
+++
@@ -1,7 +0,0 @@
-local data_exists, frmt = pcall(require, "custom.format_onsave")
-if not data_exists then
-	require("user.format_onsave")
-end
-if frmt.disable == 0 then
-	require("user.format_onsave")
-end
