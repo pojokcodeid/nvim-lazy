2e6e87a71d79dc47076276af85e5e5a51e9ff746
asep komarudin <pojokcodeid@gmail.com>
2023-02-19 16:41:10 +0700

    update

--- lua/user/format_onsave.lua
+++ lua/user/format_onsave.lua
@@ -1,22 +1,33 @@
-function format_filter(client)
-	local filetype = vim.bo.filetype
-	local n = require("null-ls")
-	local s = require("null-ls.sources")
-	local method = n.methods.FORMATTING
-	local available_formatters = s.get_available(filetype, method)
-
-	if #available_formatters > 0 then
-		return client.name == "null-ls"
-	elseif client.supports_method("textDocument/formatting") then
-		return true
-	else
-		return false
-	end
+local run = 0
+local data_exists, frmt = pcall(require, "custom.format_onsave")
+if not data_exists then
+	run = 1
 end
-vim.cmd([[
+if frmt.disable == 0 then
+	run = 1
+end
+
+if run == 1 then
+	function format_filter(client)
+		local filetype = vim.bo.filetype
+		local n = require("null-ls")
+		local s = require("null-ls.sources")
+		local method = n.methods.FORMATTING
+		local available_formatters = s.get_available(filetype, method)
+
+		if #available_formatters > 0 then
+			return client.name == "null-ls"
+		elseif client.supports_method("textDocument/formatting") then
+			return true
+		else
+			return false
+		end
+	end
+	vim.cmd([[
   augroup _lsp
        autocmd!
        " autocmd BufWritePre * lua vim.lsp.buf.format{timeout_ms =200, filter=format_filter}
        autocmd BufWritePre * lua vim.lsp.buf.format{timeout_ms=1000 ,filter=format_filter}
     augroup end
   ]])
+end
