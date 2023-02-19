df0b023c73c3cc59c422742f859328bc8782f38e
asep komarudin <pojokcodeid@gmail.com>
2023-02-19 19:20:01 +0700

    update config dapui

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -143,6 +143,7 @@ return {
 	-- debuging
 	{
 		"rcarriga/nvim-dap-ui",
+		event = "BufWinEnter",
 		dependencies = "mfussenegger/nvim-dap",
 		enabled = vim.fn.has("win32") == 0,
 		config = function()
