a476e51b92f8a61a7dfc0495bf71ae3674a2a6b1
asep komarudin <pojokcodeid@gmail.com>
2023-02-20 08:38:22 +0700

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -341,7 +341,7 @@ return {
 		end,
 	},
 	-- makes some plugins dot-repeatable like leap
-	{ "tpope/vim-repeat", event = "VeryLazy" },
+	-- { "tpope/vim-repeat", event = "VeryLazy" },
 	-- better diagnostics list and others
 	{
 		"folke/trouble.nvim",
