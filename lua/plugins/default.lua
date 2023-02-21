3a4d23006773e01901d5a4503f6bd08802fa7e3c
asep komarudin <pojokcodeid@gmail.com>
2023-02-21 13:33:13 +0700

    update

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -1,5 +1,17 @@
--- require("user.options")
--- require("user.keymaps")
--- require("user.autocommands")
--- require("user.format_onsave")
+local opts, _ = pcall(require, "user.options")
+if opts then
+	require("user.options")
+end
+local key, _ = pcall(require, "user.keymaps")
+if key then
+	require("user.keymaps")
+end
+local cmd, _ = pcall(require, "user.autocommands")
+if cmd then
+	require("user.autocommands")
+end
+local onsave, _ = pcall(require, "user.format_onsave")
+if onsave then
+	require("user.format_onsave")
+end
 return {}
