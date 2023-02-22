92f288bddf2350d34da91c91ff9a765583e97315
asep komarudin <pojokcodeid@gmail.com>
2023-02-22 07:48:02 +0700

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -71,38 +71,98 @@ return {
         { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
       },
 	},
-	-- -- auto completion (dipindah supaya loading lebih cepat)
-	-- {
-	-- 	"hrsh7th/nvim-cmp",
-	-- 	version = false, -- last release is way too old
-	-- 	event = "InsertEnter",
-	-- 	dependencies = {
-	-- 		"hrsh7th/cmp-nvim-lsp",
-	-- 		"hrsh7th/cmp-buffer",
-	-- 		"hrsh7th/cmp-path",
-	-- 		"saadparwaiz1/cmp_luasnip",
-	-- 		"hrsh7th/cmp-nvim-lua",
-	-- 		{
-	-- 			"tzachar/cmp-tabnine",
-	-- 			build = build,
-	-- 			event = "BufWinEnter",
-	-- 			config = function()
-	-- 				require("user.tabnine")
-	-- 			end,
-	-- 		},
-	-- 		-- {
-	-- 		-- 	"hrsh7th/cmp-cmdline",
-	-- 		-- 	event = "BufWinEnter",
-	-- 		-- 	config = function()
-	-- 		-- 		require("user.cmdline")
-	-- 		-- 	end,
-	-- 		-- },
-	-- 	},
-	-- 	opts = function()
-	-- 		require("user.cmp")
-	-- 	end,
-	-- },
-	--
+	-- for cmp
+	{
+		"hrsh7th/nvim-cmp",
+		version = false, -- last release is way too old
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"hrsh7th/cmp-nvim-lua",
+			-- {
+			-- 	"hrsh7th/cmp-cmdline",
+			-- 	--event = "BufWinEnter",
+			-- 	event = "VeryLazy",
+			-- 	config = function()
+			-- 		require("user.cmdline")
+			-- 	end,
+			-- },
+		},
+		opts = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+
+			local check_backspace = function()
+				local col = vim.fn.col(".") - 1
+				return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+			end
+
+			return {
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expandable() then
+							luasnip.expand()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif check_backspace() then
+							fallback()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "nvim_lua" },
+				}),
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						vim_item.kind = string.format("%s", require("user.icons")["kind"][vim_item.kind])
+						vim_item.menu = ({
+							nvim_lsp = "(LSP)",
+							luasnip = "(Snippet)",
+							buffer = "(Buffer)",
+							path = "(Path)",
+						})[entry.source.name]
+						return vim_item
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				experimental = {
+					ghost_text = false,
+					native_menu = false,
+				},
+			}
+		end,
+	},
 	{
 		"neovim/nvim-lspconfig",
 		event = "BufWinEnter",
@@ -437,6 +497,22 @@ return {
 		event = "BufRead",
 		config = function()
 			require("fidget").setup()
+		end,
+	},
+	{
+		"gelguy/wilder.nvim",
+		event = "BufWinEnter",
+		config = function()
+			local wilder = require("wilder")
+			wilder.setup({ modes = { ":", "/", "?" } })
+			wilder.set_option(
+				"renderer",
+				wilder.popupmenu_renderer({
+					highlighter = wilder.basic_highlighter(),
+					left = { " ", wilder.popupmenu_devicons() },
+					right = { " ", wilder.popupmenu_scrollbar() },
+				})
+			)
 		end,
 	},
 }
