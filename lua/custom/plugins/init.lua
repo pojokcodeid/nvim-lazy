8f8665c05f277f65fe2146475706c0782abc5350
asep komarudin <pojokcodeid@gmail.com>
2023-02-23 13:52:32 +0700

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,6 @@ return {
 	--- masukan plugin tambahan disini
 	{ "folke/trouble.nvim", enabled = false }, -- ini untuk disable plugin
 	-- { "dracula/vim" },
+	-- { "sainnhe/sonokai" },
+	-- { "RRethy/nvim-base16" },
 }
