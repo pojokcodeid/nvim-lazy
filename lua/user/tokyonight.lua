30ae653cda4e8bd64cbd68ecdefbb603f8da47a4
asep komarudin <pojokcodeid@gmail.com>
2023-03-05 00:32:18 +0700

    update

--- lua/user/tokyonight.lua
+++ lua/user/tokyonight.lua
@@ -41,12 +41,17 @@ tokyonight.setup({
 
 	--- You can override specific color groups to use other groups or a hex color
 	--- function will be called with a ColorScheme table
-	--@param colors ColorScheme
-	--on_colors = function(colors) end,
+	---@param colors ColorScheme
+	on_colors = function(colors) end,
 
 	--- You can override specific highlights to use other groups or a hex color
 	--- function will be called with a Highlights and ColorScheme table
 	--@param highlights Highlights
-	--@param colors ColorScheme
-	--on_highlights = function(highlights, colors) end,
+	---@param colors ColorScheme
+	on_highlights = function(highlights, colors)
+		highlights.NvimTreeFolderIcon = {
+			bg = colors.none,
+			fg = "#C79868",
+		}
+	end,
 })
