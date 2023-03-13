68087b6dc38be85eae2afba55427d42cf5bce410
asep komarudin <pojokcodeid@gmail.com>
2023-03-13 22:55:09 +0700

    fix bug tab

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -72,6 +72,18 @@ return {
 						"i",
 						"s",
 					}),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -57,13 +57,13 @@ return {
 		},
       -- stylua: ignore
       keys = {
-        {
-          "<tab>",
-          function()
-            return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-          end,
-          expr = true, silent = true, mode = "i",
-        },
+        -- {
+        --   "<tab>",
+        --   function()
+        --     return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
+        --   end,
+        --   expr = true, silent = true, mode = "i",
+        -- },
         { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
         { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
       },
