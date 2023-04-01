4e0481320c2c45462ebc7c71488c58499aa44306
asep komarudin <pojokcodeid@gmail.com>
2023-04-01 14:23:51 +0700

    update

--- lua/custom/default.lua
+++ lua/custom/default.lua
@@ -12,7 +12,7 @@
 -- catppuccin, catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 -- dracula
 -- nightfox, dayfox, dawnfox, duskfox, nordfox, terafox, carbonfox
-vim.g.pcode_colorscheme = "dracula"
+vim.g.pcode_colorscheme = "material_palenight"
 
 -- custom transparent mode
 -- only support
