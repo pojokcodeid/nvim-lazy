5addd87e7a18d714f7fc217cce96c7594827ddea
asep komarudin <pojokcodeid@gmail.com>
2023-06-22 13:51:39 +0700

    update

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -1,21 +1,21 @@
 vim.g.codeium_disable_bindings = 1
 return {
-	-- "Exafunction/codeium.vim",
-	-- enabled = true,
-	-- event = "InsertEnter",
-	-- config = function()
-	-- 	-- Change '<C-g>' here to any keycode you like.
-	-- 	vim.keymap.set("i", "<C-g>", function()
-	-- 		return vim.fn["codeium#Accept"]()
-	-- 	end, { expr = true })
-	-- 	vim.keymap.set("i", "<c-;>", function()
-	-- 		return vim.fn["codeium#CycleCompletions"](1)
-	-- 	end, { expr = true })
-	-- 	vim.keymap.set("i", "<c-,>", function()
-	-- 		return vim.fn["codeium#CycleCompletions"](-1)
-	-- 	end, { expr = true })
-	-- 	vim.keymap.set("i", "<c-x>", function()
-	-- 		return vim.fn["codeium#Clear"]()
-	-- 	end, { expr = true })
-	-- end,
+	"Exafunction/codeium.vim",
+	enabled = true,
+	event = "InsertEnter",
+	config = function()
+		-- Change '<C-g>' here to any keycode you like.
+		vim.keymap.set("i", "<C-g>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true })
+		vim.keymap.set("i", "<c-;>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true })
+		vim.keymap.set("i", "<c-,>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true })
+		vim.keymap.set("i", "<c-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true })
+	end,
 }
