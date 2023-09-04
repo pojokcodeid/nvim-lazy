488d78fe94701c9d8e2588d85fad7d202882c283
asep komarudin <pojokcodeid@gmail.com>
2023-09-04 15:18:56 +0700

    update

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -10,7 +10,99 @@ end
 
 local icons = require("user.icons")
 nvim_tree.setup({
-  renderer = {
+	filters = {
+		dotfiles = false,
+		git_clean = false,
+		no_buffer = false,
+		custom = { "node_modules", "\\.cache" },
+		exclude = {},
+	},
+	filesystem_watchers = {
+		enable = true,
+		debounce_delay = 50,
+		ignore_dirs = {},
+	},
+	git = {
+		enable = false, -- false dulu karena muncul error
+		ignore = true, -- true dulu karena muncul error
+		show_on_dirs = true,
+		show_on_open_dirs = true,
+		timeout = 200,
+	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		expand_all = {
+			max_folder_discovery = 300,
+			exclude = {},
+		},
+		file_popup = {
+			open_win_config = {
+				col = 1,
+				row = 1,
+				relative = "cursor",
+				border = "shadow",
+				style = "minimal",
+			},
+		},
+		open_file = {
+			quit_on_open = false,
+			resize_window = false,
+			window_picker = {
+				enable = true,
+				picker = "default",
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "lazy", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
+		},
+		remove_file = {
+			close_window = true,
+		},
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
+	live_filter = {
+		prefix = "[FILTER]: ",
+		always_show_folders = true,
+	},
+	tab = {
+		sync = {
+			open = false,
+			close = false,
+			ignore = {},
+		},
+	},
+	notify = {
+		threshold = vim.log.levels.INFO,
+	},
+	log = {
+		enable = false,
+		truncate = false,
+		types = {
+			all = false,
+			config = false,
+			copy_paste = false,
+			dev = false,
+			diagnostics = false,
+			git = false,
+			profile = false,
+			watcher = false,
+		},
+	},
+	system_open = {
+		cmd = nil,
+		args = {},
+	},
+	renderer = {
 		root_folder_modifier = ":t",
 		indent_markers = {
 			enable = true,
@@ -56,7 +148,7 @@ nvim_tree.setup({
 			},
 		},
 	},
-  diagnostics = {
+	diagnostics = {
 		enable = true,
 		show_on_dirs = true,
 		icons = {
@@ -66,7 +158,7 @@ nvim_tree.setup({
 			error = icons.diagnostics.BoldError,
 		},
 	},
-  view = {
+	view = {
 		width = 30,
 		hide_root_folder = false,
 		side = "left",
