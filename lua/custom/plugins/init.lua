084e121fc959cd74da64054a7a8c57d01ca9d9a1
asep komarudin <pojokcodeid@gmail.com>
2023-09-05 18:09:31 +0700

    update

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -120,19 +120,19 @@ return {
 	-- 		require("telescope").load_extension("laravel")
 	-- 	end,
 	-- },
-	--
-	{
-		"barrett-ruth/live-server.nvim",
-		build = "yarn global add live-server",
-		config = true,
-		init = function()
-			require("live-server").setup({
-				-- Arguments passed to live-server via `vim.fn.jobstart()`
-				-- Run `live-server --help` to see list of available options
-				-- For example, to use port 7000 and browser firefox:
-				args = { "--port=7000", "--browser=firefox" },
-				--args = {},
-			})
-		end,
-	},
+	-- --
+	-- {
+	-- 	"barrett-ruth/live-server.nvim",
+	-- 	build = "yarn global add live-server",
+	-- 	config = true,
+	-- 	init = function()
+	-- 		require("live-server").setup({
+	-- 			-- Arguments passed to live-server via `vim.fn.jobstart()`
+	-- 			-- Run `live-server --help` to see list of available options
+	-- 			-- For example, to use port 7000 and browser firefox:
+	-- 			args = { "--port=7000", "--browser=firefox" },
+	-- 			--args = {},
+	-- 		})
+	-- 	end,
+	-- },
 }
