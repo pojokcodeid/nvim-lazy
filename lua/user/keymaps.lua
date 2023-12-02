90f281d542f73c72d16e84b6c41a546a1108edd3
asep komarudin <pojokcodeid@gmail.com>
2023-12-02 21:05:28 +0700

    add CTRL + A

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -24,6 +24,7 @@ keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
+keymap("n", "<C-A>", "ggVG", opts)
 
 -- Resize with arrows
 -- keymap("n", "<C-Up>", ":resize -2<CR>", opts)
