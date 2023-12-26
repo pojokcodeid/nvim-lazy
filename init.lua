aad95a29a8ea1b8f7d34e9704a00f4b08f23371e
asep komarudin <pojokcodeid@gmail.com>
2023-12-26 11:19:42 +0700

    update neovim-vscode config

--- init.lua
+++ init.lua
@@ -1,5 +1,13 @@
 if vim.g.vscode then
-	vim.cmd([[source C:\\Users\\Asep\\AppData\\Local\\nvim\\settings.vim]])
+	Map = vim.keymap.set
+	Cmd = vim.cmd
+	VSCodeNotify = vim.fn.VSCodeNotify
+	VSCodeCall = vim.fn.VSCodeCall
+
+	-- require("vscode.options")
+	require("vscode.functions")
+	require("vscode.mappings")
+-- vim.cmd([[source C:\\Users\\Asep\\AppData\\Local\\nvim\\settings.vim]])
 else
 	require("core")
 end
---
+++ lua/vscode/mappings.lua
@@ -0,0 +1,48 @@
+-- n = normal mode
+-- i = insert mode
+-- v = visual mode
+-- x = visual block mode
+-- t = terminal mode
+-- c = command mode
+-- o = operator pending mode
+
+-- Map("", "L", Move_to_bottom_screen)
+-- Map("", "H", Move_to_top_screen)
+-- Map("", "<Space>", Trim__save__no_highlight)
+Map({ "n", "x" }, "<Space>", Active_whichkey)
+-- Map({ "n", "x" }, "<C-j>", Navigation_down)
+-- Map({ "n", "x" }, "<C-k>", Navigation_up)
+-- Map({ "n", "x" }, "<C-h>", Navigation_left)
+-- Map({ "n", "x" }, "<C-l>", Navigation_right)
+Map("", "U", Trim__save__format)
+
+Map("n", "gD", Reveal_definition_aside)
+Map("n", "gt", Go_to_implementation)
+Map("n", "gq", Go_to_reference)
+Map("n", ",r", Rename_symbol)
+Map("n", "<<", Outdent)
+Map("n", ">>", Indent)
+Map("n", "gcc", Comment)
+Map("n", "=s", Convert_to_spaces)
+Map("n", "=t", Convert_to_tabs)
+Map("n", "=d", Indent_with_spaces)
+Map("n", "=g", Indent_with_tabs)
+Map("n", "K", CloseEditor)
+Map("n", ",K", UndoCloseEditor)
+Map("n", "zk", Git_stage_file)
+Map("n", "zK", Git_unstage_file)
+Map("n", "zi", Git_open_changes)
+Map("n", "zI", Git_open_all_changes)
+Map("n", "zy", Toggle_breakpoint)
+Map("n", "yr", Copy_relative_path)
+Map("n", "yR", Copy_path)
+
+Map("v", "gs", Codesnap)
+Map("v", "<", Outdent_vis)
+Map("v", ">", Indent_vis)
+Map("v", "gc", Comment_vis)
+
+Map({ "n", "v" }, "zJ", Git_revert_change)
+Map({ "n", "v" }, "zj", Git_stage_change)
+-- Map({ "n", "v" }, "zt", Vscode_ctrl_d)
+-- Map({ "n", "v" }, "zb", Vscode_ctrl_u)
