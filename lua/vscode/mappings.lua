8254e41ef54d52c565f8e1d2b592f2197ffe6247
asep komarudin <pojokcodeid@gmail.com>
2023-12-28 22:31:43 +0700

    update

--- lua/vscode/mappings.lua
+++ lua/vscode/mappings.lua
@@ -45,3 +45,30 @@ Map({ "n", "v" }, "zJ", Git_revert_change)
 Map({ "n", "v" }, "zj", Git_stage_change)
 -- Map({ "n", "v" }, "zt", Vscode_ctrl_d)
 -- Map({ "n", "v" }, "zb", Vscode_ctrl_u)
+
+vim.api.nvim_exec(
+	[[
+    " THEME CHANGER
+    function! SetCursorLineNrColorInsert(mode)
+        " Insert mode: blue
+        if a:mode == "i"
+            call VSCodeNotify('nvim-theme.insert')
+
+        " Replace mode: red
+        elseif a:mode == "r"
+            call VSCodeNotify('nvim-theme.replace')
+        endif
+    endfunction
+
+    augroup CursorLineNrColorSwap
+        autocmd!
+        autocmd ModeChanged *:[vV\x16]* call VSCodeNotify('nvim-theme.visual')
+        autocmd ModeChanged *:[R]* call VSCodeNotify('nvim-theme.replace')
+        autocmd InsertEnter * call SetCursorLineNrColorInsert(v:insertmode)
+        autocmd InsertLeave * call VSCodeNotify('nvim-theme.normal')
+        autocmd CursorHold * call VSCodeNotify('nvim-theme.normal')
+        autocmd ModeChanged [vV\x16]*:* call VSCodeNotify('nvim-theme.normal')
+    augroup END
+]],
+	false
+)
