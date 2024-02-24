38df56571ad6e148f1d54ff8b17b410e3d014a68
asep komarudin <pojokcodeid@gmail.com>
2024-02-24 22:02:53 +0700

    fix: transparant background

--- lua/custom/plugins/transparant.lua
+++ lua/custom/plugins/transparant.lua
@@ -1,25 +1,35 @@
 return {
-	-- "xiyaowong/transparent.nvim",
-	-- event = "BufWinEnter",
-	-- cmd = { "TransparentToggle", "TransparentEnable", "TransparentDisable" },
-	-- config = function()
-	-- 	require("transparent").clear_prefix("BufferLine")
-	-- 	require("transparent").clear_prefix("Lualine")
-	-- 	require("transparent").setup({
-	-- 		extra_groups = {},
-	-- 		exclude_groups = {
-	-- 			-- disable active selection backgroun
-	-- 			"CursorLine",
-	-- 			"CursorLineNR",
-	-- 			"CursorLineSign",
-	-- 			"CursorLineFold",
-	-- 			"Pmenu",
-	-- 			"PmenuSel",
-	-- 			"PmenuSbar",
-	-- 			"PmenuThumb",
-	-- 			"NormalFloat",
-	-- 			"FloatBorder",
-	-- 		},
-	-- 	})
-	-- end,
+	-- transparant config
+	-- {
+	-- 	"xiyaowong/transparent.nvim",
+	-- 	event = "BufWinEnter",
+	-- 	cmd = { "TransparentToggle", "TransparentEnable", "TransparentDisable" },
+	-- 	config = function()
+	-- 		require("transparent").clear_prefix("BufferLine")
+	-- 		-- clear prefix for which-key
+	-- 		require("transparent").clear_prefix("WhichKey")
+	-- 		-- clear prefix for lazy.nvim
+	-- 		require("transparent").clear_prefix("Lazy")
+	-- 		-- clear prefix for NvimTree
+	-- 		require("transparent").clear_prefix("NvimTree")
+	-- 		-- clear prefix for NeoTree
+	-- 		require("transparent").clear_prefix("NeoTree")
+	-- 		-- clear prefix for Lualine
+	-- 		require("transparent").clear_prefix("Lualine")
+	-- 		require("transparent").setup({
+	-- 			extra_groups = {},
+	-- 			exclude_groups = {
+	-- 				-- disable active selection backgroun
+	-- 				"CursorLine",
+	-- 				"CursorLineNR",
+	-- 				"CursorLineSign",
+	-- 				"CursorLineFold",
+	-- 				-- disable nvimtree CursorLine
+	-- 				"NvimTreeCursorLine",
+	-- 				-- disable Neotree CursorLine
+	-- 				"NeoTreeCursorLine",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 }
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -214,40 +214,6 @@ if transparent_mode ~= nil then
 end
 
 return {
-	-- transparant config
-	{
-		"xiyaowong/transparent.nvim",
-		event = "BufWinEnter",
-		enabled = transparent,
-		cmd = { "TransparentToggle", "TransparentEnable", "TransparentDisable" },
-		config = function()
-			require("transparent").clear_prefix("BufferLine")
-			-- clear prefix for which-key
-			require("transparent").clear_prefix("WhichKey")
-			-- clear prefix for lazy.nvim
-			require("transparent").clear_prefix("Lazy")
-			-- clear prefix for NvimTree
-			require("transparent").clear_prefix("NvimTree")
-			-- clear prefix for NeoTree
-			require("transparent").clear_prefix("NeoTree")
-			-- clear prefix for Lualine
-			require("transparent").clear_prefix("Lualine")
-			require("transparent").setup({
-				extra_groups = {},
-				exclude_groups = {
-					-- disable active selection backgroun
-					"CursorLine",
-					"CursorLineNR",
-					"CursorLineSign",
-					"CursorLineFold",
-					-- disable nvimtree CursorLine
-					"NvimTreeCursorLine",
-					-- disable Neotree CursorLine
-					"NeoTreeCursorLine",
-				},
-			})
-		end,
-	},
 	-- color scheme
 	{ "luisiacc/gruvbox-baby", lazy = true, enabled = gruvbox },
 	{
