554d68c7ae546a6c445f487651e4b1af586a03e7
asep komarudin <pojokcodeid@gmail.com>
2024-02-29 20:33:22 +0700

    add: update config

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -1,6 +1,1 @@
--- custom key maps disini
-local function map(mode, l, r, desc)
-	vim.keymap.set(mode, l, r, { desc = desc })
-end
--- click ctrl + Left mouse buttn to run
-map("t", "<C-LeftMouse>", "<cmd>lua print('Run Link')<cr>", "Run Link")
+
