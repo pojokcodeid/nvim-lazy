f99a244c369300155fcd23eda59e994001b35342
asep komarudin <pojokcodeid@gmail.com>
2024-03-05 23:18:08 +0700

    add: auto cmd for create folder and file

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -43,6 +43,19 @@ vim.cmd([[
     " allows you to use Ctrl-c on terminal window
     autocmd TermOpen * nnoremap <buffer> <C-c> i<C-c>
   augroup END
+
+  function s:MkNonExDir(file, buf)
+    if empty(getbufvar(a:buf, '&buftype')) && a:file!~#'\v^\w+\:\/'
+        let dir=fnamemodify(a:file, ':h')
+        if !isdirectory(dir)
+            call mkdir(dir, 'p')
+        endif
+    endif
+    endfunction
+    augroup BWCCreateDir
+    autocmd!
+    autocmd BufWritePre * :call s:MkNonExDir(expand('<afile>'), +expand('<abuf>'))
+  augroup END
 ]])
 
 -- for fix error last close buffer
