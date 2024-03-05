73fb95878ba7e2b5408aa430bcd27eac6a4ae150
asep komarudin <pojokcodeid@gmail.com>
2024-03-05 18:12:48 +0700

    add: default title tab wezterm

--- lua/custom/autocmd.lua
+++ lua/custom/autocmd.lua
@@ -9,7 +9,7 @@ end
 -- get folder name from current directory
 local _get_folder_name = function()
 	local str = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
-	return "My " .. str:lower():gsub("^%l", string.upper)
+	return " 󱧶  " .. str:lower():gsub("^%l", string.upper) .. " "
 end
 
 local term_program = vim.fn.getenv("TERM_PROGRAM")
