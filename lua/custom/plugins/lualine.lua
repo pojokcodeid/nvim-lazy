0f8682626803d8ea14f0f83bcc3e61c3eee4702b
asep komarudin <pojokcodeid@gmail.com>
2024-03-09 15:05:09 +0700

    add: config dracula theme

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -142,10 +142,58 @@ return {
 			if data_ok then
 				lsp_progress = lspprogress.progress
 			end
+      -- stylua: ignore
+      local colors = {
+        blue              = '#9ece6a',
+        cyan              = '#bb9af7',
+        black             = '#1a1b26',
+        black_transparant = 'none',
+        white             = '#c6c6c6',
+        red               = "#ff757f",
+        skyblue           = '#7aa2f7',
+        grey              = '#3b4261',
+        yellow            = "#ffc777",
+        fg_gutter         = "#3b4261",
+        green1            = "#4fd6be",
+      }
 
+			local bubbles_theme = {
+				normal = {
+					a = { fg = colors.black, bg = colors.skyblue },
+					b = { fg = colors.white, bg = colors.grey },
+					c = { fg = colors.white, bg = colors.black_transparant },
+				},
+
+				insert = {
+					a = { fg = colors.black, bg = colors.blue },
+					b = { fg = colors.blue, bg = colors.grey },
+				},
+				visual = {
+					a = { fg = colors.black, bg = colors.cyan },
+					b = { fg = colors.cyan, bg = colors.grey },
+				},
+				replace = {
+					a = { bg = colors.red, fg = colors.black },
+					b = { bg = colors.fg_gutter, fg = colors.red },
+				},
+				command = {
+					a = { bg = colors.yellow, fg = colors.black },
+					b = { bg = colors.fg_gutter, fg = colors.yellow },
+				},
+				terminal = {
+					a = { bg = colors.green1, fg = colors.black },
+					b = { bg = colors.fg_gutter, fg = colors.green1 },
+				},
+				inactive = {
+					a = { fg = colors.white, bg = colors.black_transparant },
+					b = { fg = colors.white, bg = colors.black_transparant },
+					c = { fg = colors.black, bg = colors.black_transparant },
+				},
+			}
 			require("lualine").setup({
 				options = {
-					theme = "auto",
+					theme = bubbles_theme,
+					-- theme = "auto",
 					component_separators = { left = "", right = "" },
 					section_separators = { left = "", right = "" },
 					disabled_filetypes = {
