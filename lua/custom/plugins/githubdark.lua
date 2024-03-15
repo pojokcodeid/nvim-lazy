f0b06ee7805efa899b5a8166efcfcf7ccddd4830
asep komarudin <pojokcodeid@gmail.com>
2024-03-16 06:33:56 +0700

    add: github nvim theme config

--- lua/custom/plugins/githubdark.lua
+++ lua/custom/plugins/githubdark.lua
@@ -1,69 +1,84 @@
 return {
-	-- "projekt0n/github-nvim-theme",
-	-- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	-- priority = 1000, -- make sure to load this before all the other start plugins
-	-- config = function()
-	-- 	require("github-theme").setup({
-	-- 		options = {
-	-- 			-- Compiled file's destination location
-	-- 			compile_path = vim.fn.stdpath("cache") .. "/github-theme",
-	-- 			compile_file_suffix = "_compiled", -- Compiled file suffix
-	-- 			hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
-	-- 			hide_nc_statusline = true, -- Override the underline style for non-active statuslines
-	-- 			transparent = false, -- Disable setting background
-	-- 			terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-	-- 			dim_inactive = false, -- Non focused panes set to alternative background
-	-- 			module_default = true, -- Default enable value for modules
-	-- 			styles = { -- Style to be applied to different syntax groups
-	-- 				comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
-	-- 				functions = "NONE",
-	-- 				keywords = "NONE",
-	-- 				variables = "NONE",
-	-- 				conditionals = "NONE",
-	-- 				constants = "NONE",
-	-- 				numbers = "NONE",
-	-- 				operators = "NONE",
-	-- 				strings = "NONE",
-	-- 				types = "NONE",
-	-- 			},
-	-- 			inverse = { -- Inverse highlight for different types
-	-- 				match_paren = false,
-	-- 				visual = false,
-	-- 				search = false,
-	-- 			},
-	-- 			darken = { -- Darken floating windows and sidebar-like windows
-	-- 				floats = false,
-	-- 				sidebars = {
-	-- 					enabled = true,
-	-- 					list = {}, -- Apply dark background to specific windows
-	-- 				},
-	-- 			},
-	-- 			modules = { -- List of various plugins and additional options
-	-- 				-- ...
-	-- 			},
-	-- 		},
-	-- 		palettes = {},
-	-- 		specs = {},
-	-- 		groups = {
-	-- 			-- As with specs and palettes, the values defined under `all` will be applied to every style.
-	-- 			all = {
-	-- 				-- If `link` is defined it will be applied over any other values defined
-	-- 				Whitespace = { link = "Comment" },
-	--
-	-- 				-- Specs are used for the template. Specs have their palette's as a field that can be accessed
-	-- 				IncSearch = { bg = "palette.cyan" },
-	-- 				illuminatedWord = { bg = "#3b4261" },
-	-- 				illuminatedCurWord = { bg = "#3b4261" },
-	-- 				IlluminatedWordText = { bg = "#3b4261" },
-	-- 				IlluminatedWordRead = { bg = "#3b4261" },
-	-- 				IlluminatedWordWrite = { bg = "#3b4261" },
-	-- 				-- NvimTreeNormal = { fg = "palette.fg1", bg = "palette.bg2" },
-	-- 			},
-	-- 			github_dark = {
-	-- 				-- As with specs and palettes, a specific style's value will be used over the `all`'s value.
-	-- 				PmenuSel = { bg = "#73daca", fg = "bg0" },
-	-- 			},
-	-- 		},
-	-- 	})
-	-- end,
+	"projekt0n/github-nvim-theme",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		local palette = require("github-theme.palette").load("github_dark_dimmed")
+		require("github-theme").setup({
+			options = {
+				-- Compiled file's destination location
+				compile_path = vim.fn.stdpath("cache") .. "/github-theme",
+				compile_file_suffix = "_compiled", -- Compiled file suffix
+				hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
+				hide_nc_statusline = true, -- Override the underline style for non-active statuslines
+				transparent = false, -- Disable setting background
+				terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+				dim_inactive = false, -- Non focused panes set to alternative background
+				module_default = true, -- Default enable value for modules
+				styles = { -- Style to be applied to different syntax groups
+					comments = "italic", -- Value is any valid attr-list value `:help attr-list`
+					functions = "italic",
+					keywords = "NONE",
+					variables = "NONE",
+					conditionals = "NONE",
+					constants = "NONE",
+					numbers = "NONE",
+					operators = "NONE",
+					strings = "NONE",
+					types = "NONE",
+				},
+				inverse = { -- Inverse highlight for different types
+					match_paren = false,
+					visual = false,
+					search = false,
+				},
+				darken = { -- Darken floating windows and sidebar-like windows
+					floats = false,
+					sidebars = {
+						enabled = true,
+						list = {}, -- Apply dark background to specific windows
+					},
+				},
+				modules = { -- List of various plugins and additional options
+					-- ...
+				},
+			},
+			palettes = {
+				github_dark_dimmed = {
+					bg0 = "bg1",
+				},
+			},
+			specs = {},
+			groups = {
+				all = {
+					illuminatedWord = { bg = "#3b4261" },
+					illuminatedCurWord = { bg = "#3b4261" },
+					IlluminatedWordText = { bg = "#3b4261" },
+					IlluminatedWordRead = { bg = "#3b4261" },
+					IlluminatedWordWrite = { bg = "#3b4261" },
+					["@tag.attribute"] = { fg = "#faa356", style = "italic" },
+					["@text.uri"] = { fg = palette.const, style = "italic" },
+					["@tag"] = { fg = "#fa7970" },
+				},
+				github_dark_high_contrast = {
+					NvimTreeSpecialFile = { fg = "#faa356", style = "italic" },
+				},
+				github_dark_dimmed = {
+					-- As with specs and palettes, a specific style's value will be used over the `all`'s value.
+					NvimTreeNormal = { fg = "fg1", bg = "bg1" },
+					NvimTreeSpecialFile = { fg = "#faa356", style = "italic" },
+					BufferLineFill = { bg = "bg1" },
+					BufferLineUnfocusedFill = { bg = "bg1" },
+					LualineNormal = { bg = "bg1" },
+					StatusLine = { bg = "bg1" },
+					StatusLineTerm = { bg = "bg1" },
+					Pmenu = { bg = "bg1" },
+					PmenuSel = { link = "CursorLine" },
+					WhichKeyFloat = { bg = "bg1" },
+					LazyNormal = { bg = "bg1" },
+					LazyBackground = { bg = "bg1" },
+				},
+			},
+		})
+	end,
 }
