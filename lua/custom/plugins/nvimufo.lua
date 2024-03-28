5518885b31ff2b3a8d097f5601f9c32b00b03afc
asep.komarudin <asep.komarudin@dataon.com>
2024-03-28 18:40:37 +0700

    add: update config ufo

--- lua/custom/plugins/nvimufo.lua
+++ lua/custom/plugins/nvimufo.lua
@@ -17,7 +17,7 @@ return {
 			end,
 		},
 	},
-	event = "BufRead",
+	event = "BufReadPost",
 	config = function()
 		vim.o.foldcolumn = "1" -- '0' is not bad
 		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
@@ -28,7 +28,7 @@ return {
 		-- these are "extra", change them as you like
 		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
 		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
-
+		vim.cmd("highlight FoldColumn guifg=" .. vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Comment")), "fg"))
 		-- Option 3: treesitter as a main provider instead
 		-- Only depend on `nvim-treesitter/queries/filetype/folds.scm`,
 		-- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
