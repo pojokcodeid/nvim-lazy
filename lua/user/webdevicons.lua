d5d7c8f3f995ce5587330d752f4e24e13c3115ee
asep.komarudin <asep.komarudin@dataon.com>
2024-04-29 07:11:30 +0700

    add: update

--- lua/user/webdevicons.lua
+++ lua/user/webdevicons.lua
@@ -7,6 +7,9 @@ local material_icon_ok, material_icon = pcall(require, "nvim-material-icon")
 if not material_icon_ok then
 	return
 end
+material_icon.setup({
+	override = {},
+})
 
 web_devicons.setup({
 	override = material_icon.get_icons(),
