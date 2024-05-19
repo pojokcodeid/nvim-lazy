63de9b207477241bb2bfaf065ba4f7d14e34e7e7
asep.komarudin <asep.komarudin@dataon.com>
2024-05-19 22:25:01 +0700

    add: update ts rainbow for neovim v 0.10

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -394,11 +394,43 @@ return {
 	-- for check startuptime
 	{ "dstein64/vim-startuptime", lazy = true, cmd = "StartupTime" },
 	-- {
-	-- 	"HiPhish/nvim-ts-rainbow2",
+	-- 	"pojokcodeid/nvim-ts-rainbow2",
 	-- 	lazy = true,
 	-- 	event = "BufRead",
 	-- 	dependencies = "nvim-treesitter/nvim-treesitter",
 	-- },
+	{
+		"hiphish/rainbow-delimiters.nvim",
+		lazy = true,
+		event = "BufRead",
+		config = function()
+			-- Modul ini berisi beberapa definisi default
+			local rainbow_delimiters = require("rainbow-delimiters")
+			vim.g.rainbow_delimiters = {
+				strategy = {
+					[""] = rainbow_delimiters.strategy["global"],
+					vim = rainbow_delimiters.strategy["local"],
+				},
+				query = {
+					[""] = "rainbow-delimiters",
+					lua = "rainbow-blocks",
+				},
+				priority = {
+					[""] = 110,
+					lua = 210,
+				},
+				highlight = {
+					"RainbowDelimiterRed",
+					"RainbowDelimiterYellow",
+					"RainbowDelimiterBlue",
+					"RainbowDelimiterOrange",
+					"RainbowDelimiterGreen",
+					"RainbowDelimiterViolet",
+					"RainbowDelimiterCyan",
+				},
+			}
+		end,
+	},
 	{
 		"lewis6991/gitsigns.nvim",
 		lazy = true,
--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -34,11 +34,11 @@ configs.setup({
 	-- 	equery = "rainbow-parens",
 	-- 	strategy = require("ts-rainbow").strategy.global,
 	-- },
-	rainbow = {
-		enable = false,
-		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-	},
+	-- rainbow = {
+	-- 	enable = false,
+	-- 	extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+	-- 	max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
+	-- },
 	-- autotag = { enable = true, enable_rename = true, enable_close = true, enable_close_on_slash = true },
 	incremental_selection = { enable = true },
 	indent = { enable = true, disable = { "python", "css" } },
