377c23c156d4358b239a07279fd5f7ba50130107
asep.komarudin <asep.komarudin@dataon.com>
2024-05-21 14:34:49 +0700

    add: update config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,9 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
-    lazypath })
+  vim.fn.system({
+    "git", "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath
+  })
 end
 
 local icons = require("user.icons").ui
