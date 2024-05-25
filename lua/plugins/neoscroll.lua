2e251f9723791cfc5c53d39e0e34b3733ddaa016
asep.komarudin <asep.komarudin@dataon.com>
2024-05-25 18:50:20 +0700

    fix: error config neoscroll

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,8 +1,8 @@
 return {
-	"karb94/neoscroll.nvim",
-	event = "VeryLazy",
-	lazy = true,
-	config = function()
-		require("neoscroll").setup()
-	end,
+  "karb94/neoscroll.nvim",
+  event = "VeryLazy",
+  lazy = true,
+  config = function()
+    require("neoscroll").setup {}
+  end,
 }
