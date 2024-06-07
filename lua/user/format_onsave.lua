520ae101c6f5ba3431dfc1c4ba191fd0b8686816
asep.komarudin <asep.komarudin@dataon.com>
2024-06-07 07:02:06 +0700

    fix : #8 warning [LSP][null-ls] timeout

--- lua/user/format_onsave.lua
+++ lua/user/format_onsave.lua
@@ -1,38 +1,38 @@
 local run = 0
 local frmt = vim.g.pcode_format_on_save or 0
 if frmt == 1 then
-	run = 1
+  run = 1
 else
-	run = 0
+  run = 0
 end
 
 local buf_clients = vim.lsp.get_clients()
 if next(buf_clients) == nil then
-	run = 0
+  run = 0
 end
 
 if run == 1 then
-	function FORMAT_FILTER(client)
-		local filetype = vim.bo.filetype
-		local n = require("null-ls")
-		local s = require("null-ls.sources")
-		local method = n.methods.FORMATTING
-		local available_formatters = s.get_available(filetype, method)
+  function FORMAT_FILTER(client)
+    local filetype = vim.bo.filetype
+    local n = require "null-ls"
+    local s = require "null-ls.sources"
+    local method = n.methods.FORMATTING
+    local available_formatters = s.get_available(filetype, method)
 
-		if #available_formatters > 0 then
-			return client.name == "null-ls"
-		elseif client.supports_method("textDocument/formatting") then
-			return true
-		else
-			return false
-		end
-	end
+    if #available_formatters > 0 then
+      return client.name == "null-ls"
+    elseif client.supports_method "textDocument/formatting" then
+      return true
+    else
+      return false
+    end
+  end
 
-	vim.cmd([[
+  vim.cmd [[
   augroup _lsp
        autocmd!
        " autocmd BufWritePre * lua vim.lsp.buf.format{timeout_ms =200, filter=format_filter}
-       autocmd BufWritePre * lua vim.lsp.buf.format{timeout_ms=2000 ,filter=FORMAT_FILTER}
+       autocmd BufWritePre * lua vim.lsp.buf.format{timeout_ms=5000 ,filter=FORMAT_FILTER}
     augroup end
-  ]])
+  ]]
 end
