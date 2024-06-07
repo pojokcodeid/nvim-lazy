56e8da9c8e4eaa1a11df7badd0723aabafafec74
asep.komarudin <asep.komarudin@dataon.com>
2024-06-07 18:53:54 +0700

    enc: dashboard quit info

--- lua/user/dashboard.lua
+++ lua/user/dashboard.lua
@@ -1,35 +1,35 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-	return
+  return
 end
 
 local board = {
-	[[             _       _                    _      ]],
-	[[            (_)     | |                  | |     ]],
-	[[ _ __   ___  _  ___ | | __   ___ ___   __| | ___ ]],
-	[[| '_ \ / _ \| |/ _ \| |/ /  / __/ _ \ / _` |/ _ \]],
-	[[| |_) | (_) | | (_) |   <  | (_| (_) | (_| |  __/]],
-	[[| .__/ \___/| |\___/|_|\_\  \___\___/ \__,_|\___|]],
-	[[| |        _/ |                                  ]],
-	[[|_|       |__/                                   ]],
+  [[             _       _                    _      ]],
+  [[            (_)     | |                  | |     ]],
+  [[ _ __   ___  _  ___ | | __   ___ ___   __| | ___ ]],
+  [[| '_ \ / _ \| |/ _ \| |/ /  / __/ _ \ / _` |/ _ \]],
+  [[| |_) | (_) | | (_) |   <  | (_| (_) | (_| |  __/]],
+  [[| .__/ \___/| |\___/|_|\_\  \___\___/ \__,_|\___|]],
+  [[| |        _/ |                                  ]],
+  [[|_|       |__/                                   ]],
 }
 
 local data_board = vim.g.pcode_header2
 if data_board ~= nil then
-	board = data_board
+  board = data_board
 end
 
-local dashboard = require("alpha.themes.dashboard")
+local dashboard = require "alpha.themes.dashboard"
 dashboard.section.header.val = board
 dashboard.section.buttons.val = {
-	dashboard.button("F", "󰈞  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "󰉋  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "󰦛  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
-	dashboard.button("z", "󰒲  Lazy", ":Lazy<CR>"),
-	dashboard.button("q", "󰅚  Quit Neovim", ":qa<CR>"),
+  dashboard.button("F", "󰈞  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  dashboard.button("p", "󰉋  Find project", ":Telescope projects <CR>"),
+  dashboard.button("r", "󰦛  Recently used files", ":Telescope oldfiles <CR>"),
+  dashboard.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
+  dashboard.button("z", "󰒲  Lazy", ":Lazy<CR>"),
+  dashboard.button("q", "󰅚  Quit", ":qa<CR>"),
 }
 
 dashboard.section.footer.opts.hl = "Type"
@@ -43,27 +43,27 @@ alpha.setup(dashboard.opts)
 local footer_text = "Pojok Code"
 local data_txt = vim.g.pcode_footer
 if data_txt ~= nil then
-	footer_text = data_txt
+  footer_text = data_txt
 end
 
 vim.api.nvim_create_autocmd("User", {
-	pattern = "LazyVimStarted",
-	desc = "Add Alpha dashboard footer",
-	once = true,
-	callback = function()
-		local stats = require("lazy").stats()
-		local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
-		dashboard.section.footer.val =
-			{ footer_text .. " " .. stats.loaded .. "/" .. stats.count .. " plugins  in " .. ms .. "ms" }
-		pcall(vim.cmd.AlphaRedraw)
-	end,
+  pattern = "LazyVimStarted",
+  desc = "Add Alpha dashboard footer",
+  once = true,
+  callback = function()
+    local stats = require("lazy").stats()
+    local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
+    dashboard.section.footer.val =
+      { footer_text .. " " .. stats.loaded .. "/" .. stats.count .. " plugins  in " .. ms .. "ms" }
+    pcall(vim.cmd.AlphaRedraw)
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "User" }, {
-	pattern = { "AlphaReady" },
-	callback = function()
-		vim.cmd([[
+  pattern = { "AlphaReady" },
+  callback = function()
+    vim.cmd [[
       set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
-    ]])
-	end,
+    ]]
+  end,
 })
--- lua/user/startify.lua
+++ lua/user/startify.lua
@@ -1,28 +1,28 @@
-local alpha = require("alpha")
-local startify = require("alpha.themes.startify")
+local alpha = require "alpha"
+local startify = require "alpha.themes.startify"
 local dash_model = {}
 dash_model = {
-	[[                _      __                __    ]],
-	[[    ___ ___    (____  / /__  _______ ___/ ___  ]],
-	[[   / _ / _ \  / / _ \/  '_/ / __/ _ / _  / -_) ]],
-	[[  / .__\_____/ /\___/_/\_\  \__/\___\_,_/\__/  ]],
-	[[ /_/      |___/                                ]],
+  [[                _      __                __    ]],
+  [[    ___ ___    (____  / /__  _______ ___/ ___  ]],
+  [[   / _ / _ \  / / _ \/  '_/ / __/ _ / _  / -_) ]],
+  [[  / .__\_____/ /\___/_/\_\  \__/\___\_,_/\__/  ]],
+  [[ /_/      |___/                                ]],
 }
 
 local board = vim.g.pcode_header1
 if board ~= nil then
-	dash_model = board
+  dash_model = board
 end
 
 startify.section.header.val = dash_model
 startify.section.top_buttons.val = {
-	startify.button("F", "󰈞  Find file", ":Telescope find_files <CR>"),
-	startify.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	startify.button("p", "󰉋  Find project", ":Telescope projects <CR>"),
-	startify.button("r", "󰦛  Recently used files", ":Telescope oldfiles <CR>"),
-	startify.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
-	startify.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
-	startify.button("z", "󰒲  Lazy", ":Lazy<CR>"),
+  startify.button("F", "󰈞  Find file", ":Telescope find_files <CR>"),
+  startify.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+  startify.button("p", "󰉋  Find project", ":Telescope projects <CR>"),
+  startify.button("r", "󰦛  Recently used files", ":Telescope oldfiles <CR>"),
+  startify.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
+  startify.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
+  startify.button("z", "󰒲  Lazy", ":Lazy<CR>"),
 }
 -- disable MRU
 startify.section.mru.val = { { type = "padding", val = 4 } }
@@ -34,43 +34,43 @@ startify.nvim_web_devicons.enabled = false
 -- startify.nvim_web_devicons.highlight = 'Keyword'
 --
 startify.section.bottom_buttons.val = {
-	startify.button("q", "󰅚  Quit NVIM", ":qa<CR>"),
+  startify.button("q", "󰅚  Quit", ":qa<CR>"),
 }
 
 local footer_text = "Pojok Code"
 local data_txt = vim.g.pcode_footer
 if data_txt ~= nil then
-	footer_text = data_txt
+  footer_text = data_txt
 end
 
 vim.api.nvim_create_autocmd("User", {
-	pattern = "LazyVimStarted",
-	desc = "Add Alpha dashboard footer",
-	once = true,
-	callback = function()
-		local stats = require("lazy").stats()
-		local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
-		startify.section.footer.val = {
-			{
-				type = "text",
-				val = { footer_text .. " " .. stats.loaded .. "/" .. stats.count .. " plugins  in " .. ms .. "ms" },
-			},
-		}
-		pcall(vim.cmd.AlphaRedraw)
-	end,
+  pattern = "LazyVimStarted",
+  desc = "Add Alpha dashboard footer",
+  once = true,
+  callback = function()
+    local stats = require("lazy").stats()
+    local ms = math.floor(stats.startuptime * 100 + 0.5) / 100
+    startify.section.footer.val = {
+      {
+        type = "text",
+        val = { footer_text .. " " .. stats.loaded .. "/" .. stats.count .. " plugins  in " .. ms .. "ms" },
+      },
+    }
+    pcall(vim.cmd.AlphaRedraw)
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "User" }, {
-	pattern = { "AlphaReady" },
-	callback = function()
-		vim.cmd([[
+  pattern = { "AlphaReady" },
+  callback = function()
+    vim.cmd [[
       set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3
-    ]])
-	end,
+    ]]
+  end,
 })
 -- ignore filetypes in MRU
 local default_mru_ignore = {}
 startify.mru_opts.ignore = function(path, ext)
-	return (string.find(path, "COMMIT_EDITMSG")) or (vim.tbl_contains(default_mru_ignore, ext))
+  return (string.find(path, "COMMIT_EDITMSG")) or (vim.tbl_contains(default_mru_ignore, ext))
 end
 alpha.setup(startify.config)
