17f2ffe48be698f24ef9a4e1b9e24abe24c8fac4
asep.komarudin <asep.komarudin@dataon.com>
2024-06-11 16:35:50 +0700

    add: example config database ui

--- lua/custom/plugins/database.lua
+++ lua/custom/plugins/database.lua
@@ -1,86 +1,47 @@
 return {
-  -- {
-  --   "tpope/vim-dadbod",
-  --   dependencies = {
-  --     "kristijanhusak/vim-dadbod-ui",
-  --     "kristijanhusak/vim-dadbod-completion",
-  --   },
-  --   keys = {
-  --     { "<leader>du", ":DBUIToggle<cr>", desc = "Toggle DB UI" },
-  --     { "<leader>db", ":DBUIFindBuffer<cr>", desc = "DB Find buffer" },
-  --     {
-  --       "<leader>dr",
-  --       ":DBUIRenameBuffer<cr>",
-  --       desc = "DB Rename buffer",
-  --     },
-  --     { "<leader>dl", ":DBUILastQueryInfo<cr>", desc = "DB Last query" },
-  --   },
-  --   config = function()
-  --     local function db_completion()
-  --       require("cmp").setup.buffer {
-  --         sources = { { name = "vim-dadbod-completion" } },
-  --       }
-  --     end
-  --     vim.g.db_ui_show_database_icon = 1
-  --     vim.g.db_ui_use_nerd_fonts = 1
-  --     vim.g.db_ui_show_help = 0
-  --     local expanded_icon = "▾"
-  --     local collapsed_icon = "▸"
-  --
-  --     vim.g.db_ui_icons = {
-  --       expanded = {
-  --         db = expanded_icon .. " 󰆼",
-  --         buffers = expanded_icon .. " ",
-  --         saved_queries = expanded_icon .. " ",
-  --         schemas = expanded_icon .. " ",
-  --         schema = expanded_icon .. " 󰙅",
-  --         tables = expanded_icon .. " 󰓱",
-  --         table = expanded_icon .. " ",
-  --       },
-  --       collapsed = {
-  --         db = collapsed_icon .. " 󰆼",
-  --         buffers = collapsed_icon .. " ",
-  --         saved_queries = collapsed_icon .. " ",
-  --         schemas = collapsed_icon .. " ",
-  --         schema = collapsed_icon .. " 󰙅",
-  --         tables = collapsed_icon .. " 󰓱",
-  --         table = collapsed_icon .. " ",
-  --       },
-  --       saved_query = "  ",
-  --       new_query = "  󰓰",
-  --       tables = "  󰓫",
-  --       buffers = "  ",
-  --       add_connection = "  󰆺",
-  --       connection_ok = "✓",
-  --       connection_error = "✕",
+  -- "kristijanhusak/vim-dadbod-ui",
+  -- dependencies = {
+  --   { "tpope/vim-dadbod", lazy = true },
+  --   { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+  -- },
+  -- init = function()
+  --   -- Your DBUI configuration
+  --   vim.g.db_ui_show_database_icon = 1
+  --   vim.g.db_ui_use_nerd_fonts = 1
+  --   vim.g.db_ui_show_help = 0
+  -- end,
+  -- config = function()
+  --   local function db_completion()
+  --     require("cmp").setup.buffer {
+  --       sources = { { name = "vim-dadbod-completion" } },
   --     }
-  --     vim.g.db_ui_save_location = vim.fn.stdpath "config" .. require("plenary.path").path.sep .. "db_ui"
+  --   end
+  --   vim.g.db_ui_save_location = vim.fn.stdpath "config" .. require("plenary.path").path.sep .. "db_ui"
   --
-  --     vim.api.nvim_create_autocmd("FileType", {
-  --       pattern = {
-  --         "sql",
-  --       },
-  --       command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
-  --     })
+  --   vim.api.nvim_create_autocmd("FileType", {
+  --     pattern = {
+  --       "sql",
+  --     },
+  --     command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+  --   })
   --
-  --     vim.api.nvim_create_autocmd("FileType", {
-  --       pattern = {
-  --         "sql",
-  --         "mysql",
-  --         "plsql",
-  --       },
-  --       callback = function()
-  --         vim.schedule(db_completion)
-  --       end,
-  --     })
-  --   end,
-  --   cmd = {
-  --     "DBUIToggle",
-  --     "DBUI",
-  --     "DBUIAddConnection",
-  --     "DBUIFindBuffer",
-  --     "DBUIRenameBuffer",
-  --     "DBUILastQueryInfo",
-  --   },
+  --   vim.api.nvim_create_autocmd("FileType", {
+  --     pattern = {
+  --       "sql",
+  --       "mysql",
+  --       "plsql",
+  --     },
+  --     callback = function()
+  --       vim.schedule(db_completion)
+  --     end,
+  --   })
+  -- end,
+  -- cmd = {
+  --   "DBUIToggle",
+  --   "DBUI",
+  --   "DBUIAddConnection",
+  --   "DBUIFindBuffer",
+  --   "DBUIRenameBuffer",
+  --   "DBUILastQueryInfo",
   -- },
 }
