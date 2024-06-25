f5adb4bb7d01390db2c3b1139ab6a4f800cc1b67
asep.komarudin <asep.komarudin@dataon.com>
2024-06-25 20:32:28 +0700

    enc: testing java on linux

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,9 +1,4 @@
 _G.pcode = _G.pcode or {}
-vim.filetype.add({
-  pattern = {
-    [".*%.blade%.php"] = "blade",
-  },
-})
 require("custom.default")
 require("custom.dashboard")
 require("config.lazy_lib")
@@ -25,12 +20,3 @@ require("custom.keymaps")
 -- vim.cmd("colorscheme onedark")
 -- vim.cmd("colorscheme github_dark_dimmed")
 -- vim.cmd("colorscheme github_dark_high_contrast")
-
--- local mason_reg = require("mason-registry")
--- for _, pkg in pairs(mason_reg.get_installed_packages()) do
---   for _, type in pairs(pkg.spec.categories) do
---     if type == "LSP" and pkg.spec.name == "jdtls" then
---       print("JDTLS READY")
---     end
---   end
--- end
