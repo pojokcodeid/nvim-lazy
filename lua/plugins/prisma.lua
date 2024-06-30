02f4aab2156504e0e10ef92dab63e0f3c519d96c
asep.komarudin <asep.komarudin@dataon.com>
2024-06-30 18:53:42 +0700

    add: prisma format and key mappings

--- lua/plugins/prisma.lua
+++ lua/plugins/prisma.lua
@@ -14,9 +14,40 @@ if pcode.active_prisma_config then
         opts.ensure_installed = opts.ensure_installed or {}
         vim.list_extend(opts.ensure_installed, { "prismals" })
         vim.cmd([[
-        autocmd BufWritePost *.prisma silent! execute "!npx prisma format"
-      ]])
+          autocmd BufWritePost *.prisma silent! execute "!npx prisma format"
+        ]])
       end,
+      keys = {
+        { "<leader>p", "", desc = "  Prisma" },
+        {
+          "<leader>pf",
+          function()
+            vim.cmd("execute '!npx prisma format'")
+          end,
+          desc = "Prisma Format",
+        },
+        {
+          "<leader>pm",
+          function()
+            vim.cmd("execute '!npx prisma init --datasource-provider mysql'")
+          end,
+          desc = "init MySQL",
+        },
+        {
+          "<leader>pp",
+          function()
+            vim.cmd("execute '!npx prisma init --datasource-provider postgresql'")
+          end,
+          desc = "init postgresql",
+        },
+        {
+          "<leader>pe",
+          function()
+            vim.cmd("execute '!npx prisma migrate dev --name init'")
+          end,
+          desc = "Migrate",
+        },
+      },
     },
   }
 end
