c66913e4c2008d559bd452b5402eaebe9863d64d
Pojok Code <asep.komarudin@dataon.com>
2024-11-16 21:51:22 +0700

    enc: add gradle create new project

--- lua/auto-jdtls2/init.lua
+++ lua/auto-jdtls2/init.lua
@@ -3,6 +3,7 @@ local M = {}
 M.setup = function(opt)
   opt = opt or {}
   require("auto-jdtls.utils").install()
+  require("auto-jdtls.create_gradle_project")
   require("auto-jdtls.create_maven_project")
   require("auto-jdtls.create_springboot_project")
   require("auto-jdtls.utils").attach_jdtls(opt)
