3fb9efd7c9f414f81bd5372e35e419acea9ac687
Pojok Code <asep.komarudin@dataon.com>
2024-11-19 13:03:26 +0700

    enc: update config java jdtls

--- lua/pcode/plugins/lang/java.lua
+++ lua/pcode/plugins/lang/java.lua
@@ -47,28 +47,15 @@ M = {
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
-      "rcasia/neotest-java",
+      "andy-bell101/neotest-java",
     },
-    opts = function()
-      return {
+    enabled = is_gradle(),
+    config = function()
+      require("neotest").setup({
         adapters = {
           require("neotest-java"),
         },
-      }
-    end,
-    enabled = is_gradle(),
-    config = function(_, opts)
-      -- get neotest namespace (api call creates or returns namespace)
-      local neotest_ns = vim.api.nvim_create_namespace("neotest")
-      vim.diagnostic.config({
-        virtual_text = {
-          format = function(diagnostic)
-            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
-            return message
-          end,
-        },
-      }, neotest_ns)
-      require("neotest").setup(opts)
+      })
     end,
     -- stylua: ignore
     keys = {
